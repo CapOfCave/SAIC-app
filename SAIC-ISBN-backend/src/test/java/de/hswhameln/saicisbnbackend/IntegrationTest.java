a106c90be33f53e912e07d46f126d31d6ae905ad
Linus <comm.cody2000@gmail.com>
2021-10-22 08:29:45 +0200

    fix IntegrationTest

--- SAIC-ISBN-backend/src/test/java/de/hswhameln/saicisbnbackend/IntegrationTest.java
+++ SAIC-ISBN-backend/src/test/java/de/hswhameln/saicisbnbackend/IntegrationTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import de.hswhameln.saicisbnbackend.controller.BookController;
@@ -17,13 +16,10 @@ import de.hswhameln.saicisbnbackend.dto.BookCreationDTO;
 import de.hswhameln.saicisbnbackend.services.BookService;
 import de.hswhameln.saicisbnbackend.services.ValidationService;
 import de.hswhameln.saicisbnbackend.services.ValidationService.ValidationResponse;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.containsString;
 
 /**
  * lädt die Dependencies und Testet die Anwendung an sich.
@@ -38,12 +34,12 @@ class IntegrationTest {
     private BookService bookService;
 
     @Autowired
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Test
-    @Disabled
     void integrationTestSave() throws Exception{
         when(validationService.validate(any(String.class))).thenReturn(new ValidationResponse(true, "ISBN is valid."));
-        this.mockMvc.perform(post("/book/saveBook")).bodyqueryParam(@RequestBody BookCreationDTO("Harry Potter", "J. K. Rowling", "Hamburger Carlsen Verlag", "3551551677")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(post("/book/saveBook").contentType(MediaType.APPLICATION_JSON).content(
+            "{ \"titel\": \"Harry Potter\", \"autor\": \"J. K. Rowling\", \"verlag\": \"Hamburger Carlsen Verlag\",\"isbn13\": \"3551551677\" }")).andDo(print()).andExpect(status().isOk());
         }
 }
